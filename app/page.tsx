0e9f2a6961a3f0220b4cfd1ffb25842f65520fa2
Unknown-bot-sus <albent2017@gmail.com>
2024-04-11 22:33:20 +0800

    Added social media icons to the footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { About } from "@/components/sections/about";
 import { Contact } from "@/components/sections/contact";
 import { Education } from "@/components/sections/education";
 import { Experiences } from "@/components/sections/experiences";
+import { Footer } from "@/components/sections/footer";
 import { Hero } from "@/components/sections/hero";
 import { Projects } from "@/components/sections/projects";
 import { experiences } from "@/lib/constants";
@@ -18,6 +19,7 @@ export default function Home() {
       <Projects />
       <Education />
       <Contact />
+      <Footer />
     </main>
   );
 }
--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -1,6 +1,34 @@
+import Link from "next/link";
+import { ContactIcon } from "../contact-icon";
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { TiSocialLinkedin } from "react-icons/ti";
+
 export const Footer = () => {
   return (
     <footer className=" w-full h-10 text-white">
+      <div className="py-5 bg-gray-900">
+        <ul className="mx-auto flex justify-center gap-10 items-center">
+          <li>
+            <Link href="https://www.linkedin.com/in/thar-lin-shwe/">
+              <ContactIcon className="hover:rounded-lg p-0 hover:text-4xl hover:bg-[#0077B5]">
+                <TiSocialLinkedin />
+              </ContactIcon>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://leetcode.com/Unknown-bot-sus/">
+              <ContactIcon className=""></ContactIcon>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/Unknown-bot-sus">
+              <ContactIcon className=" hover:bg-white hover:text-black hover:items-end hover:text-4xl">
+                <TbBrandGithubFilled />
+              </ContactIcon>
+            </Link>
+          </li>
+        </ul>
+      </div>
       <div className="bg-black flex justify-center items-center p-2">
         Copyright &#169; 2024; Designed by Thar Lin
       </div>
