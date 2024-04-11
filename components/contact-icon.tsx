58fced78820d367573749ca8e798f35948d77978
Unknown-bot-sus <albent2017@gmail.com>
2024-04-11 22:32:51 +0800

    Fixed border radius error for contact-icon component

--- components/contact-icon.tsx
+++ components/contact-icon.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const ContactIcon: FC<Props> = ({ children, className }) => {
   return (
     <div
-      className={`flex items-center justify-center rounded-full bg-black w-10 h-10 text-xl transition-all ease-in-out duration-500 ${className}`}
+      className={`flex items-center justify-center rounded-[20px] bg-black w-10 h-10 text-xl transition-all ease-in-out duration-500 hover:scale-110 ${className}`}
     >
       {children}
     </div>
