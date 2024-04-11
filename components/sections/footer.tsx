0ec4c5de54e3b654286d95f494959b10b42c53ff
Unknown-bot-sus <albent2017@gmail.com>
2024-04-11 21:37:36 +0800

    Implemented footer component

---
+++ components/sections/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className=" w-full h-10 text-white">
+      <div className="bg-black flex justify-center items-center p-2">
+        Copyright &#169; 2024; Designed by Thar Lin
+      </div>
+    </footer>
+  );
+};
