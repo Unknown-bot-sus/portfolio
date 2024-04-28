addc864c50101a2e19137fe03ba778e8f99f76de
Unknown-bot-sus <albent2017@gmail.com>
2024-04-28 16:27:34 +0800

    Implemented html for cert card component

--- components/cards/cert-card.tsx
+++ components/cards/cert-card.tsx
@@ -1,6 +1,30 @@
 import { ICertification } from "@/lib/types";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
 import { FC } from "react";
 
-export const CertCard: FC<ICertification> = ({}) => {
-  return <div></div>;
+export const CertCard: FC<ICertification> = ({
+  id,
+  name,
+  organization,
+  issueDate,
+  expireDate,
+  credentialId,
+  credentialUrl,
+}) => {
+  return (
+    <Link
+      href={credentialUrl}
+      target="_blank"
+      className="flex flex-col bg-background-100 rounded-xl p-5"
+    >
+      <span>{name}</span>
+      <span>{organization}</span>
+      <div>
+        <span>Issued {formatDate(issueDate)}</span>
+        {expireDate ? <span> - Expires {formatDate(expireDate)}</span> : null}
+      </div>
+      <small>{credentialId}</small>
+    </Link>
+  );
 };
