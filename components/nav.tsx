18ec0ba91897cb26b94c6afe36e1240bb0d0b120
Thar Lin Shwe <albent2017@gmail.com>
2024-10-08 19:00:44 +0800

    Implemented nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
 export const Nav = () => {
-  return <nav></nav>;
+    const links = [
+        { name: "Education", href: "#education" },
+        { name: "Skills", href: "#skills" },
+        { name: "Experience", href: "#experience" },
+        { name: "Projects", href: "#projects" },
+        { name: "Hire Me", href: "#hireme" }
+    ];
+
+    return <header className="w-full">
+        <nav className="flex justify-between">
+            <Link className="flex-1" href="/">Thar Lin</Link>
+
+            <ul className="flex-1 flex justify-between gap-5">
+                {links.map(({ name, href }) => (
+                    <li><Link href={href}>{name}</Link></li>
+                ))}
+            </ul>
+        </nav>
+    </header>
 };
