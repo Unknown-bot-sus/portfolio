7e42134f5ec3db07a6b1be37db909ae7cdb43199
Thar Lin Shwe <albent2017@gmail.com>
2024-10-09 20:53:44 +0800

    Implemented useScroll hook

---
+++ components/hooks/useScrollProgress.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export function useScrollProgress() {
+    const [scrollProgress, setScrollProgress] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+            const docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            const scrollPercent = (scrollTop / docHeight) * 100;
+            setScrollProgress(scrollPercent);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return scrollProgress;
+}
